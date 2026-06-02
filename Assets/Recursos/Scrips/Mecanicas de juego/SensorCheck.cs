79718277a544656d64e1422818c9eaa80f361c21
frankpolania1-arch <frankpolania1@gmail.com>
2026-06-02 15:58:39 -0500

    Puntos

--- Assets/Recursos/Scrips/Mecanicas de juego/SensorCheck.cs
+++ Assets/Recursos/Scrips/Mecanicas de juego/SensorCheck.cs
@@ -1,14 +1,27 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class SensorCheck : MonoBehaviour
 {
     public GameManager gameManager;
+    public Animator anim;
+
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             gameManager.OnCheckTriggered(other);
+            anim.SetBool("check", true);
+        }
+    }
+    private async void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            await Task.Delay(500);
+            anim.SetBool("check", false);
         }
     }
 }
